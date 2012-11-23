533208668e0e08ab8a486bc8689ae4abaed73be7
lulu <lulu@lulu-mac-pro.gateway.pace.com>
2012-11-22 20:14:35 -0800

    install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1275fa008cb3e774f0d7e42b6ab9b9f67f4ded82bef1c467c7c94993ff5f8ce80622e2e4d5dcf54e671a702d60a3ae5f6566cff8c24d5e01bd1d23e22d2174b6'
